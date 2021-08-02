345f3a85ecde6bee50ea34e48c41c74940883efb
Jyotsana111 <85560336+Jyotsana111@users.noreply.github.com>
2021-08-02 16:20:34 +0530

    Add files via upload

---
+++ pol.js
@@ -0,0 +1,23 @@
+class Pol {
+    constructor(x, y, width, height) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.body.scale = 0.2;
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("R.png");
+        World.add(world, this.body);
+      }
+      display(){
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        pop();
+      }
+  }
+     
